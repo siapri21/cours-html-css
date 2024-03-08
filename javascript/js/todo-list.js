4a6b411bc9847f97f9fddd1b60a9abb202faf818
siapri21 <siapriouattara21@gmail.com>
2024-03-08 16:49:48 +0100

    first commit

--- javascript/js/todo-list.js
+++ javascript/js/todo-list.js
@@ -3,9 +3,7 @@
 
 !ajouter un ecouteur d'evenement =>click
 
-
 */
-
 // verifier si au click on affiche un message en console 
 // creer un li 
 // cible le input
@@ -13,9 +11,30 @@
 // ajouter dans le ul
 
 
-const todoList = document.querySelector('button');
+const todoList = document.querySelector('.btn');
 // console.log(todoList);
 
 todoList.addEventListener('click' , function (){
+    const todoListInput = document.querySelector('#container')
     
-})
+    if( todoListInput.value != ''){
+    const title = document.createElement('li');
+    title.textContent = todoListInput.value;
+    const todoListUl = document.querySelector('#todo-list');
+    todoListUl.prepend(title);
+    }
+    
+});
+
+// ajouter un ecouteur sur l'ul =>click
+// la cible changera sur l'ul => double click
+//supprime l'element
+
+todoList.addEventListener('click' ,function(e){
+    e.target.style.textDecoration = 'line-through'
+});
+
+
+todoList.addEventListener('click', function(e){
+    e.target.remove();
+});
---
+++ javascript/pratice/formulaire.js
@@ -0,0 +1,12 @@
+const submit = document.querySelector('.submit')
+const containerInput = document.querySelector('#input-container')
+
+
+containerInput.addEventListener('click' ,function(event){
+    if(containerInput.value != ''){
+        const elementEror = document.createElement('p');
+        console.log(elementEror);
+    }
+
+})
+
